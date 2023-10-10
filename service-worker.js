2bbc32a0cf64392284bdcf71730cfd1d842feb3e
Franz Enzenhofer <franz.enzenhofer@fullstackoptimization.com>
2023-10-10 13:57:08 +0200

    fetch new version

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,30 @@
+// Service Worker Installation
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('flapimoji-cache').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
-        'https://flapimoji.franzai.com/show.jpeg'
+        'https://flapimoji.franzai.com/show.jpeg',
       ]);
     })
   );
 });
 
+// Service Worker Fetch Event
 self.addEventListener('fetch', (event) => {
+  if (navigator.onLine) { // Check if online
+    event.respondWith(fetch(event.request)); // Fetch latest version
+    return;
+  }
+  
+  // If offline, use cache
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
+
+//serviceworker 10 10 2023
